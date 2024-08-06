61de651e2a5ab3047ba1462d10ac3f0296f40a16
D-Raff <damonraffels@gmail.com>
2024-08-06 10:11:58 +0200

    map integration

---
+++ src/components/map.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="coords">
+    <button @click="getLocation()">Get Location</button>
+    {{ lat }}, {{ lng }}
+  </div>
+
+  <div ref="mapContainer" id="map"></div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const lat = ref(0);
+const lng = ref(0);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat.value = position.coords.latitude;
+      lng.value = position.coords.longitude;
+    });
+  }
+}
+</script>
+
+<style scoped>
+.coords {
+  min-height: 100vh;
+}
+#map {
+  width: 400px;
+  height: 400px;
+}
+</style>
